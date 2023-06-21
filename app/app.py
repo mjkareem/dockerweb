65257e2d7d394bc2809d92f9dca8679df2f803a5
mjkareem <mujahidakande@gmail.com>
2023-06-21 22:45:25 +0200

    adding Titanic.csv and titanic app.py

--- app/app.py
+++ app/app.py
@@ -24,5 +24,27 @@ def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
 
+@app.route('/titanic')
+def titanic():
+    count = get_hit_count()
+    titanic_data = pd.read_csv('titanic.csv')
+    titanic_data_head = titanic_data.head(5)
+
+    titanic_survived = titanic_data['survived'].value_counts()
+    titanic_survived_men = titanic_data.loc[
+        (titanic_data['survived'] == 1) & (titanic_data['sex'] == 'male')
+    ].shape[0]
+    titanic_survived_women = titanic_data.loc[
+        (titanic_data['survived'] == 1) & (titanic_data['sex'] == 'female')
+    ].shape[0]
+
+    return render_template(
+        'titanic.html',
+        count=count,
+        titanic_data=titanic_data_head,
+        titanic_survived_men=titanic_survived_men,
+        titanic_survived_women=titanic_survived_women
+    )
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
